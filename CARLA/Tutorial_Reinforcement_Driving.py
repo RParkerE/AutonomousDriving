b885bdb79f23728d35a2e681b15d9b92e68a163d
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2023-11-10 08:46:15 -0600

    Update Tutorial_Reinforcement_Driving.py
    Update ModifiedTensorBoard for 2.X

--- CARLA/Tutorial_Reinforcement_Driving.py
+++ CARLA/Tutorial_Reinforcement_Driving.py
@@ -49,46 +49,40 @@ AGGREGATE_STATS_EVERY = 10
 IM_WIDTH = 640
 IM_HEIGHT = 480
 
-# Own Tensorboard class
+
 class ModifiedTensorBoard(TensorBoard):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.step = 1
+		self.writer = tf.summary.create_file_writer(self.log_dir)
+		self._log_write_dir = self.log_dir
 
-    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.step = 1
-        self.writer = tf.summary.create_file_writer(self.log_dir)
+	def set_model(self, model):
+		self.model = model
 
-    # Overriding this method to stop creating default log writer
-    def set_model(self, model):
-        pass
+		self._train_dir = os.path.join(self._log_write_dir, 'train')
+		self._train_step = self.model._train_counter
 
-    # Overrided, saves logs with our step number
-    # (otherwise every .fit() will start writing from 0th step)
-    def on_epoch_end(self, epoch, logs=None):
-        self.update_stats(**logs)
+		self._val_dir = os.path.join(self._log_write_dir, 'validation')
+		self._val_step = self.model._test_counter
 
-    # Overrided
-    # We train for one batch only, no need to save anything at epoch end
-    def on_batch_end(self, batch, logs=None):
-        pass
+		self._should_write_train_graph = False
 
-    # Overrided, so won't close writer
-    def on_train_end(self, _):
-        pass
+	def on_epoch_end(self, epoch, logs=None):
+		self.update_stats(**logs)
 
-    # Custom method for saving own metrics
-    # Creates writer, writes custom metrics and closes writer
-    def update_stats(self, **stats):
-        self._write_logs(stats, self.step)
+	def on_batch_end(self, batch, logs=None):
+		pass
 
-    # More or less the same writer as in Keras' Tensorboard callback
-    # Physically writes to the log files
-    def _write_logs(self, logs, index):
-        with self.writer.as_default():
-            for name, value in logs.items():
-                tf.summary.scalar(name, value, step=index)
-                self.step += 1
-                self.writer.flush()
+	def on_train_end(self, _):
+		pass
+
+	def update_stats(self, **stats):
+		with self.writer.as_default():
+			for key, value in stats.items():
+				tf.summary.scalar(key, value, step = self.step)
+				self.writer.flush()
+
 class CarEnv:
 	SHOW_CAM = SHOW_PREVIEW
 	STEER_AMT = 1.0
